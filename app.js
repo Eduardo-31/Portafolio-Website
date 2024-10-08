639ea79751572466eb4eaaea433eb74ed1e98a88
Eduardo <thelincoln31@gmail.com>
2024-10-07 20:04:19 -0500

    configure dark and email

--- app.js
+++ app.js
@@ -15,26 +15,23 @@ links.addEventListener('click', () => {
 
 
 
-const sendBtn = document.querySelector(".send-btn")
+const sendBtn = document.getElementById("form")
 
-sendBtn.addEventListener('click', (e) => {
-      const name = document.querySelector(".name")
-      const email = document.querySelector(".email")
-      const asunto = document.querySelector(".asunto")
+sendBtn.addEventListener('submit', (e) => {
       e.preventDefault()
-      if(name.value == "" || email.value == "" || asunto.value == ""){
+      const name = document.querySelector(".name").value.replace(/\s+/g, ' ').trim();
+      const email = document.querySelector(".email").value.replace(/\s+/g, ' ').trim();
+      const asunto = document.querySelector(".asunto").value.replace(/\s+/g, ' ').trim();
+      if(!name || !email || !asunto ){
          console.log('sin value')
          emptyerror();
       } else {
          console.log('con value')
-         sendEmail(name.value, email.value, asunto.value);
+         sendEmail(name, email, asunto);
          success();
       }
    })
    
-
-
-
 
 
 function sendEmail(name, email, asunto) {
@@ -48,25 +45,26 @@ function sendEmail(name, email, asunto) {
 function emptyerror() { 
    swal({
       title: "Oh No....",
-      text: "Fields cannot be empty!",
+      text: "Los campos no pueden estar vacios.",
       icon: "error"
    })
 }
 
 function success() { 
    swal({
-      title: "Email sent succesfully",
-      text: "We try to reply in 24 hours",
+      //title: "Email sent succesfully",
+      title: "Email enviado con éxito",
+      //text: "We try to reply in 24 hours",
+      text: "Intentaremos responder a la brevedad.",
       icon: "success"
    })
 }
 
-
+// esto no es un button es un div
 const switchBtn = document.getElementById('switch')
 
 switchBtn.addEventListener('click', () => {
       switchBtn.classList.toggle('switch-active')
-   document.body.classList.toggle('dark')
-  
+      document.body.classList.toggle('dark')
 })
 
